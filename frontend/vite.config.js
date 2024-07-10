03215aee1ac37cea5c95572421cbcb3bde72ccc4
TaskManagement <surajkushwaha8272@gmail.com>
2024-07-10 23:10:22 +0530

    add

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,8 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server :{
  proxy :{
-   '/api':'http://localhost:3000',
-  // '/api':'https://fullstackpratice.onrender.com',
+  //  '/api':'http://localhost:3000',
+   '/api':'https://fullstackpratice.onrender.com',
  },
 },
   plugins: [react()],
